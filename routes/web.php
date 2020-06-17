14a48150c4a9cbab06193ad6791c9b4efe4f42f0
Spashev <spashev.n@codeguru.be>
2020-06-17 21:25:17 +0600

    adminlte3

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/about', 'HomeController@about')->name('about');
 Route::get('/contacts', 'HomeController@contact')->name('contact');
 
+Route::name('admin.')->prefix('admin')->namespace('Admin')->group(function () {
+    Route::get('/dashboard', 'AdminController@index')->name('index');
+    Route::get('/login', function () {
+        // Route assigned name 'admin.users'...
+    })->name('login');
+});
